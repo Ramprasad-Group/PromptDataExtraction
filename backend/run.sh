387f660c15b9e4de40cd4d1225ab9d7df8e6a570
akhlakm <me@akhlakm.com>
2023-08-01 15:20:43 -0400

    Update run script for 0 to 5 shots

--- backend/run.sh
+++ backend/run.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # DATASET can take value Tg or bandgap
-# SAMPLING can take the value random or error_diversity
+# SAMPLING can take the value random or error_diversity or baseline_diversity
 
-OUT_DIR="RunOut"
+OUT_DIR="Run_Output"
 DATASET="Tg"
-SAMPLING="random"
-EXPERIMENTBASE="test_5_$SAMPLING"
-ERROR_FILE="$OUT_DIR/output/${DATASET}/prompt_5_0_shot/llm_error_doi_list.json"
+SAMPLING="error_diversity"
+EXPERIMENTBASE="abstracts_$SAMPLING"
+ERROR_FILE="$OUT_DIR/output/${DATASET}/llm_error_doi_list.json"
 echo $EXPERIMENTBASE
 echo $ERROR_FILE
-start=1
-end=1
+
+start=5
+end=0
+
+mkdir -p $OUT_DIR/log $OUT_DIR/output $OUT_DIR/data
 
 # Iterate over the range of numbers
-for ((count=start; count<=end; count++)); do
-
+for ((count=start; count>=end; count--)); do
+    echo
     echo "Few shot count: ${count}"
-    LOGFILE="$OUT_DIR/log/${DATASET}_${EXPERIMENTBASE}_${count}_shot.log"
-    EXPERIMENTNAME="${EXPERIMENTBASE}_${count}_shot"
+    LOGFILE="$OUT_DIR/log/${DATASET}_${EXPERIMENTBASE}_${count}shot.log"
+    EXPERIMENTNAME="${EXPERIMENTBASE}_${count}shot"
+
     echo "Log file location: ${LOGFILE}"
     echo "Name of experiment: ${EXPERIMENTNAME}"
-
-    mkdir -p $OUT_DIR/log $OUT_DIR/output $OUT_DIR/data
 
     python prompt_extraction/run_inference.py \
                 --experiment_name $EXPERIMENTNAME \
@@ -29,9 +31,8 @@ for ((count=start; count<=end; count++)); do
                 --mode $DATASET \
                 --out_dir $OUT_DIR \
                 --prompt_index 5 \
-                --debug true \
                 --seed_sampling $SAMPLING \
-                --seed_count $count | tee $LOGFILE
+                --seed_count $count &> $LOGFILE
 done
 
 echo "Done"
