fa0e3eca76977c54752ed2aad1cbcfe75062be1d
akhlakm <me@akhlakm.com>
2023-08-03 15:20:17 -0400

    Update to run for all abstracts

--- backend/run_openai_reprod.sh
+++ backend/run_openai_reprod.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Run 0 to 5 shots data extraction 3 times to check for reproducibility.
 
-OUT_DIR="Run_Reprod"
+OUT_DIR="Run_Reprod_all"
 
 # can take value Tg or bandgap
 DATASET="Tg"
 
 # SAMPLING can take the value random or error_diversity or baseline_diversity
-SAMPLING="baseline_diversity"
+SAMPLING="error_diversity"
 
-EXPERIMENTBASE="abstr_${SAMPLING}_prompt0"
+EXPERIMENTBASE="abstr_${SAMPLING}_prompt5"
 
 ERROR_FILE="$OUT_DIR/output/${DATASET}/llm_error_doi_list.json"
 echo $EXPERIMENTBASE
@@ -45,9 +45,7 @@ for ((run=1; run<=3; run++)); do
                     --doi_error_list_file $ERROR_FILE \
                     --mode $DATASET \
                     --out_dir $OUT_DIR \
-                    --prompt_index 0 \
-                    --debug_count 30 \
-                    --debug true \
+                    --prompt_index 5 \
                     --seed_sampling $SAMPLING \
                     --seed_count $count &> $LOGFILE
     done
