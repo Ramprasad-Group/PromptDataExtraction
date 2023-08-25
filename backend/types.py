200150a0cdc4550d1e7e06372b3677a19013a407
akhlakm <me@akhlakm.com>
2023-08-25 18:39:25 -0400

    Add property type

--- backend/types.py
+++ backend/types.py
@@ -2,10 +2,12 @@ from dataclasses import dataclass
 
 @dataclass
 class NerTag:
-    """ NER Tag object.
+    """
+    NER Tag object.
+    
     Attributes:
-        text    (str): The original text
-        label   (str): The named entity
+        text    : The original text
+        label   : The named entity
     """
     text : str
     label : str
@@ -16,12 +18,31 @@ class NerLabelGroup:
     """
     Joined NER tag object that combines consecutive tags.
 
-    text    (str): The original text
-    label   (str): The name of the entity
-    start   (int): Start index of the entity
-    end     (int): End index of the entity
+    text    : The original text
+    label   : The name of the entity
+    start   : Start index of the entity
+    end     : End index of the entity
     """
     text : str
     label : str
     start : int
     end : int 
+
+
+@dataclass
+class Property:
+    """
+    Property object.
+
+    Attributes:
+        value    : The numerical value
+        unit     : The unit
+        text     : Text representation
+        error    : Standard error if any
+        relation : Any relationship of the value, eg. <, >, ~ etc.
+    """
+    value : float
+    unit : str
+    text : str
+    error : float
+    relation : str
