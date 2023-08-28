9741a22d57b185c13810e303ab0dfc019296f572
akhlakm <me@akhlakm.com>
2023-08-28 10:39:21 -0400

    Add text normalization modules

---
+++ backend/text/normalize.py
@@ -0,0 +1,95 @@
+import re
+from backend.types import Property
+
+
+RE_NUMBER = r'[+-]?\d+[.]?\d*(?:\s?±\s?\d+[.]?\d*)?(?:x10\^{[-]?\d*})?'
+RE_EXP = r'10\^{[-]?\d*}'
+RE_EXP_UNIT = r'([a-zA-Z]+\^{[-]?\d*})'
+VALUE_RELATIONS = ['<', '>', '~', '=', 'and', '≈', 'to', '-']
+
+
+def to_numeric(value : str) -> float:
+    """ Convert a string to float by taking scientific notations into
+        account.
+        Returns the float representation.
+
+        value :
+            The string to convert to float.
+    """
+    if '^{' in value and '}' in value and 'x' in value:
+        mantissa, exponent = value.split('x')[0].strip(), value.split('x')[1].strip().replace('10^{', '').replace('}', '')
+        numeric_value = float(mantissa)*10**(float(exponent))
+        return numeric_value
+    elif '^{' in value and '}' in value and 'x' not in value:
+        exponent = value.strip().replace('10^{', '').replace('}', '')
+        numeric_value = 10**(float(exponent))
+        return numeric_value
+    else:
+        return float(value)
+
+
+def cleanup_property(prop : Property) -> Property:
+    """ Cleanup the value and unit text of a property.
+        Return the cleaned Property.
+    """
+    value = str(prop.value)
+
+    # Property value needs some pre-processing to replace exponents
+    # in units that could match a number.
+    # Might also have to split on - to capture the negative sign.
+    value = re.sub(r'(\d)-(\d)', '\\1 - \\2', value)
+    value = re.sub(r'(\d) (\d)', '\\1\\2', value)
+    value = re.sub(r'(\d) x (\d)', '\\1x\\2', value)
+
+    units_to_replace = re.findall(RE_EXP_UNIT, value)
+    units_dict = dict()
+
+    for i, unit in enumerate(units_to_replace):
+        units_dict[f'AAA{chr(i+64)}'] = unit
+        value = value.replace(unit, f'AAA{chr(i+64)}')
+
+    numeric_values = re.findall(RE_NUMBER, value)
+    # print(numeric_values)
+
+    for value in numeric_values:
+        if '±' in value:
+            values = value.split('±')
+            prop.value = to_numeric(values[0].strip())
+            prop.error = to_numeric(values[-1].strip())
+            break
+    else:
+        if numeric_values:
+            prop.value = sum([to_numeric(num) for num in numeric_values])/(len(numeric_values))
+        else:
+            prop.value = None
+    
+    # Deal with cases like 10^{7} not covered by our regular expressions..
+    # Hack solution, find a way to integrate this with our regular expression
+    if '10^{' in value and not any(['10^{' in value for value in numeric_values]):
+        numeric_values = re.findall(RE_EXP, value)
+        if numeric_values:
+            prop.value = sum([to_numeric(num) for num in numeric_values])/(len(numeric_values))
+        else:
+            prop.value = None
+
+    
+    prop.numeric_value_avg = len(numeric_values) > 1
+    leftover_str = value
+    for value in numeric_values:
+        leftover_str = leftover_str.replace(value, '')
+
+    # Replace units back
+    prop.relation = ''
+
+    for str_item in VALUE_RELATIONS:
+        if str_item in leftover_str:
+            prop.relation = prop.relation + str_item
+            leftover_str = leftover_str.replace(str_item, '')
+
+    for key, value in units_dict.items():
+        leftover_str = leftover_str.replace(key, value)
+
+    prop.unit = leftover_str.strip()
+    # unit_conversion(prop)
+    return prop
+
---
+++ backend/text/unit.py
@@ -0,0 +1,200 @@
+"""
+Module for unit normalization.
+
+"""
+from backend.types import Property
+
+
+def normalize_unit(prop : Property) -> Property:
+    """ Normalize the unit and value of a Property.
+
+        @Todo: add more units to this.
+    """
+
+    # Remove trailing period
+    if prop.unit and prop.unit[-1] in ['.']:
+        prop.unit = prop.unit[:-1].strip()
+
+    if prop.value is not None:
+        assert type(prop.value) == float, "Property value must be float"
+
+        if prop.unit == 'K':
+            prop.value -= 273
+            prop.unit = '° C'
+        elif prop.unit in ['kPa', 'KPa']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'MPa'
+        elif prop.unit == 'GPa':
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'MPa'
+        elif prop.unit in ['mS/cm', 'mS cm^{-1}', 'mS / cm', 'mS*cm^{-1}']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'S/cm'
+        elif prop.unit in ['S/m', 'S m^{-1}']:
+            prop.value /= 100
+            prop.property_numeric_error /= 100
+            prop.unit = 'S cm^{-1}'
+        elif prop.unit in ['mV']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'V'
+        elif prop.unit in ['kg/mol', 'kg mol^{-1}','KDa', 'kDa']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000 
+            prop.unit = 'g/mol'
+        elif prop.unit in ['mW/mK', 'mW m^{-1} K^{-1}', 'mW/m*K', 'mW*m^{-1}*K^{-1}', 'mW/(m*K)', 'mW/m K', 'mW/m * K']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'W m^{-1} K^{-1}'
+        elif prop.unit in ['kW kg^{-1}', 'kW/kg', 'kW*kg^{-1}', 'W g^{-1}']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'W kg^{-1}'
+        elif prop.unit in ['kW g^{-1}']:
+            prop.value *= 1000000
+            prop.property_numeric_error *= 1000000
+            prop.unit = 'W kg^{-1}'
+        elif prop.unit in ['mA g^{-1}', 'mA/g', 'mA*g^{-1}', 'mAg^{-1}']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'A g^{-1}'
+        elif prop.unit in ['μA cm^{-2}', 'μA/cm^{2}', 'μA*cm^{-2}', 'uA cm^{-2}']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'mA cm^{-2}'
+        elif prop.unit in ['mA m^{-2}', 'mA/m^{2}']:
+            prop.value /= 10000
+            prop.property_numeric_error /= 10000
+            prop.unit = 'mA cm^{-2}'
+        elif prop.unit in ['A/m^{2}', 'A m^{-2}']:
+            prop.value /= 10
+            prop.property_numeric_error /= 10
+            prop.unit = 'mA cm^{-2}'
+        elif prop.unit in ['nA/cm^{2}', 'nA cm^{-2}']:
+            prop.value /= 1000000
+            prop.property_numeric_error /= 1000000
+            prop.unit = 'mA cm^{-2}'
+        elif prop.unit in ['A*cm^{-2}', 'A cm^{-2}']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'mA cm^{-2}'
+        elif prop.unit in ['mW m^{-2}', 'mW/m^{2}', 'mW*m^{-2}', 'mWm^{-2}']:
+            prop.value /= 10000
+            prop.property_numeric_error /= 10000
+            prop.unit = 'mW cm^{-2}'
+        elif prop.unit in ['W cm^{-2}', 'W/cm^{2}', 'Wcm^{-2}']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'mW cm^{-2}'
+        elif prop.unit in ['μW cm^{-2}', 'μW/cm^{2}', 'uW cm^{-2}', 'μW*cm^{-2}', 'μWcm^{-2}', 'uW/cm^{2}', 'μ W/cm^{2}']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'mW cm^{-2}'
+        elif prop.unit in ['W/m^{2}', 'W m^{-2}', 'μW.mm^{-2}', 'μW mm^{-2}']:
+            prop.value /= 10
+            prop.property_numeric_error /= 10
+            prop.unit = 'mW cm^{-2}'
+        elif prop.unit in ['mW/mm^{2}']:
+            prop.value *= 100
+            prop.property_numeric_error *= 100
+            prop.unit = 'mW cm^{-2}'
+        elif prop.unit in ['kW/cm^{2}']:
+            prop.value *= 1000000
+            prop.property_numeric_error *= 1000000
+            prop.unit = 'mW cm^{-2}'
+        elif prop.unit in ['cm^{3}(STP) cm/cm^{2} s cmHg', 'cm^{3}(STP) cm/(cm^{2} s cmHg)']:
+            prop.value *= 10**10
+            prop.property_numeric_error *= 10**10
+            prop.unit = 'Barrer'
+        elif prop.unit in ['mol m m^{-2} s^{-1} Pa^{-1}( barrer)', 'mol m m^{-2} s^{-1} Pa^{-1}']:
+            prop.value *= float(10**16)/3.35
+            prop.property_numeric_error *= float(10**16)/3.35
+            prop.unit = 'Barrer'
+        elif prop.unit in ['μg g^{-1}', 'μg/g']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'mg/g'
+        elif prop.unit in ['g g^{-1}', 'g/g', 'g/ g']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'mg/g'
+        elif prop.unit in ['kg m^{-3}', 'kg/m^{3}']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'g/cm^{3}'
+        elif prop.unit in ['μM', 'uM', 'μmol L^{-1}']:
+            prop.value /= 10**6
+            prop.property_numeric_error /= 10**6
+            prop.unit = 'M'
+        elif prop.unit in ['nM', 'nmol L^{-1}']:
+            prop.value /= 10**9
+            prop.property_numeric_error /= 10**9
+            prop.unit = 'M'
+        elif prop.unit in ['pM',]:
+            prop.value /= 10**15
+            prop.property_numeric_error /= 10**15
+            prop.unit = 'M'
+        elif prop.unit in ['mM',]:
+            prop.value /= 10**3
+            prop.property_numeric_error /= 10**3
+            prop.unit = 'M'
+        elif prop.unit in ['mg/cm^{3}', 'mg cm^{-3}', 'g/dm^{3}', 'g/L', 'mg/cc', 'kg*m^{-3}', 'mg*cm^{-3}', 'kgm^{-3}']:
+            prop.value /= 10**3
+            prop.property_numeric_error /= 10**3
+            prop.unit = 'g/cm^{3}'
+        elif prop.unit in ['kcal/mol', 'kcal mol^{-1}', 'kcal/mole']:
+            prop.value *= 4.18
+            prop.property_numeric_error *= 4.18
+            prop.unit = 'kJ/mol'
+        elif prop.unit in ['μA μM^{-1} cm^{-2}', 'μA cm^{-2} μM^{-1}', 'uA uM^{-1} cm^{-2}', 'mA mM^{-1} cm^{-2}', 'mA cm^{-2} mM^{-1}', 'μAμM^{-1}cm^{-2}']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'μA mM^{-1} cm^{-2}'
+        elif prop.unit in ['nA mM^{-1} cm^{-2}']:
+            prop.value /= 1000
+            prop.property_numeric_error /= 1000
+            prop.unit = 'μA mM^{-1} cm^{-2}'
+        elif prop.unit in ['Pa*s', 'Pa.s', 'Pa s', 'Pas', 'Pa s^{-1}']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'mPa s'
+        elif prop.unit in ['kΩ/sq', 'kΩ/ #', 'kΩ sq^{-1}', 'kΩ/square']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'Ω/sq'
+        elif prop.unit in ['MV/cm']:
+            prop.value *= 1000
+            prop.property_numeric_error *= 1000
+            prop.unit = 'kV/mm'
+        elif prop.unit in ['kV/cm']:
+            prop.value /= 10
+            prop.property_numeric_error /= 10
+            prop.unit = 'kV/mm'
+        elif prop.unit in ['Pa']:
+            prop.value /= 1000000
+            prop.property_numeric_error /= 1000000
+            prop.unit = 'MPa'
+        elif prop.unit in ['μΩ cm', 'μΩ*cm', 'μΩcm']:
+            prop.value /= 1000000
+            prop.property_numeric_error /= 1000000
+            prop.unit = 'Ω cm'
+        elif prop.unit in ['Ω m', 'Ωm']:
+            prop.value *= 100
+            prop.property_numeric_error *= 100
+            prop.unit = 'Ω cm'
+        elif prop.unit in ['L m^{-2} h^{-1} MPa^{-1}', 'L*m^{-2}*h^{-1}*MPa^{-1}']:
+            prop.value *= 100
+            prop.property_numeric_error *= 100
+            prop.unit = 'Ω cm'
+        elif prop.unit in ['μW cm^{-1} K^{-2}', 'uW cm^{-1} K^{-2}', 'μW/cm⋅K^{2}', 'uW/cmK^{2}', 'μWcm^{-1} K^{-2}', 'μWcm^{-1}K^{-2}']:
+            prop.value *= 100
+            prop.property_numeric_error *= 100
+            prop.unit = 'μW m^{-1} K^{-2}'
+        elif prop.unit == '' and prop.value<=1.0 and prop.value>=0.0 and prop.entity_name in self.convert_fraction_to_percentage:
+            prop.value *= 100
+            prop.unit = '%'
+        
+    return prop
