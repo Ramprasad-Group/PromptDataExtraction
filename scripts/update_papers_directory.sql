e03dd9e2330e831e9c76a020e2c420de09d0b5f1
akhlakm <me@akhlakm.com>
2023-09-12 10:17:40 -0400

    Add scripts to update directory name on postgres

---
+++ scripts/update_papers_directory.sql
@@ -0,0 +1,16 @@
+update papers set directory = 'aip' where publisher = 'AIP Publishing';
+update papers set directory = 'acs' where publisher = 'American Chemical Society (ACS)';
+update papers set directory = 'elsevier' where publisher = 'Elsevier';
+update papers set directory = 'elsevier' where publisher = 'Elsevier BV';
+update papers set directory = 'hindawi' where publisher = 'Hindawi Limited';
+update papers set directory = 'informa' where publisher = 'Informa UK Limited';
+update papers set directory = 'rsc' where publisher = 'Royal Society of Chemistry (RSC)';
+update papers set directory = 'springer' where publisher = 'Springer Berlin Heidelberg';
+update papers set directory = 'springer' where publisher = 'Springer International Publishing';
+--update papers set directory = '' where publisher = 'Springer Nature';
+update papers set directory = 'springer' where publisher = 'Springer Netherlands';
+update papers set directory = 'springer' where publisher = 'Springer New York';
+update papers set directory = 'springer' where publisher = 'Springer Science and Business Media LLC';
+update papers set directory = 'springer' where publisher = 'Springer Singapore';
+update papers set directory = 'springer' where publisher = 'Springer US';
+update papers set directory = 'wiley' where publisher = 'Wiley';
