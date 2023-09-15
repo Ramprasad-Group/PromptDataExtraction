dea0707ea1fc9c0d76a44005ddca28478a8f2c32
akhlakm <me@akhlakm.com>
2023-09-15 08:19:12 -0400

    Fix sett and imports

--- frontend/resources.py
+++ frontend/resources.py
@@ -1,6 +1,6 @@
-import streamlit as st
 import pylogg
-import sett
+import streamlit as st
+from backend import sett
 
 logger = pylogg.New('res')
 sett.load_settings()
@@ -11,15 +11,14 @@ def postgres():
     from backend import postgres
     postgres.load_settings()
     db = postgres.connect()
-    logger.info("PostGres connected.")
     return db
 
 @st.cache_resource
 def materials_bert():
     """ Return the materials bert pipeline handler. """
-    from backend.nlp import ner
-    bert = ner.MaterialsBERT(sett.NerModel.model)
-    bert.init_local_model(sett.NerModel.pytorch_device)
+    from backend.record_extraction import bert_model
+    bert = bert_model.MaterialsBERT(sett.NERPipeline.model)
+    bert.init_local_model(device=sett.NERPipeline.pytorch_device)
     return bert
 
 @st.cache_resource
