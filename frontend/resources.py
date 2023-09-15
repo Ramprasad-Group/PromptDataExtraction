85007b820fccc4a3f4389d15c1883d15c241a35b
akhlakm <me@akhlakm.com>
2023-09-15 10:56:18 -0400

    Cache ner json files

--- frontend/resources.py
+++ frontend/resources.py
@@ -26,3 +26,13 @@ def selected_properties():
     """ Class to handle selected list of properties. """
     from backend.data.properties import LLMProperties
     return LLMProperties()
+
+@st.cache_resource
+def ner_files():
+    """ Return the NEN and prop metadata files required by NER. """
+    from backend.record_extraction import utils
+    # Load NEN dataset and property metadata.
+    normdata = utils.LoadNormalizationDataset(sett.DataFiles.polymer_nen_json)
+    norm_dataset = normdata.process_normalization_files()
+    prop_metadata = utils.load_property_metadata(sett.DataFiles.properties_json)
+    return norm_dataset, prop_metadata
