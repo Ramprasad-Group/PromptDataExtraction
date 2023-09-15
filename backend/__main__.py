a10e707c4b8b83b377ec4c3902e9f0b7d48df81a
akhlakm <me@akhlakm.com>
2023-09-15 10:00:00 -0400

    Add subparser and console module

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,22 +1,24 @@
-import os, sys
+import os
 import argparse
 import pylogg as log
 from backend import postgres, sett
 
-COMMANDS = [
-    'cursor',
-    'metric',
-    'migrate',
-]
+from backend.console import (
+    calculate_metrics
+)
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('command', help="Name of the command",
-                        choices=COMMANDS)
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    calculate_metrics.add_args(subparsers)
+
     args = parser.parse_args()
     return args
 
-def init(args):
+
+def main():
+    args = parse_args()
     sett.load_settings()
     os.makedirs(sett.Run.directory, exist_ok=True)
 
@@ -24,18 +26,16 @@ def init(args):
         log_level=sett.Run.logLevel,
         output_directory=sett.Run.directory
     )
-
     log.setMaxLength(1000)
 
     postgres.load_settings()
-    db = postgres.connect()
+
+    if args.command == 'metric':
+        calculate_metrics.run(args)
 
     t1.done("All done.")
     log.close()
 
-def main():
-    args = parse_args()
-    init(args)
 
 if __name__ == "__main__":
     main()
---
+++ backend/console/calculate_metrics.py
@@ -0,0 +1,44 @@
+import json
+import argparse
+
+import pylogg
+from backend import sett, postgres
+from backend.metrics import curated
+
+log = pylogg.New("calc_metrics")
+
+tg_corefs = [
+    'Tg', 'T_{g}', 'T_{g}s', 'T_{g})',
+    'glass transition temperature',
+    'glass transition temperature T_{g}',
+    'the glass transition temperature',
+    'glass transition', 'glass transition temperatures',
+]
+
+eg_corefs = [
+    'bandgap', 'band gap', 'band gaps', 'E_{g}',
+    'optical band gap', 'optical bandgap',
+]
+
+def add_args(subparsers : argparse._SubParsersAction):
+    """ Add module specific arguments. """
+    parser = subparsers.add_parser(
+        'metric', help='Calculate metrics using curated data table.')
+    parser.add_argument('value', help="Name of the value")
+
+def run(args : argparse.ArgumentParser):
+    db = postgres.connect()
+    t1 = log.info("Calculating metrics.")
+
+    return
+
+    metrics = curated.compute_metrics(db, tg_corefs, 'materials-bert')
+    with open(sett.Run.directory + "/tg_metrics.json", "w") as fp:
+        json.dump(metrics, fp)
+    t1.done("Metrics: {}", metrics)
+
+    metrics = curated.compute_metrics(db, eg_corefs, 'materials-bert')
+    with open(sett.Run.directory + "/eg_metrics.json", "w") as fp:
+        json.dump(metrics, fp)
+
+    t1.done("Metrics: {}", metrics)
