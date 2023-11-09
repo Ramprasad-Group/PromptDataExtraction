7901b6031a108d1d6e075f5d4617532fd7dce26d
akhlakm <me@akhlakm.com>
2023-11-08 23:21:59 -0500

    Add remove param to delete existing rows

--- backend/post_process/validator.py
+++ backend/post_process/validator.py
@@ -43,7 +43,26 @@ class DataValidator:
             log.info("Unprocessed Row IDs: {} to {}",
                     records[0].id, records[-1].id)
         return records
-    
+
+    def _delete_existing(self):
+        query = self._get_existing_sql()
+        postgres.raw_sql(query, commit=True,
+            filter=self.filter_name, table=self.table_name, mid=self.method.id)
+        log.warn("Deleted existing '{}' {} items for filtered_data.",
+                 self.filter_name, self.table_name)
+
+    def _get_existing_sql(self) -> str:
+        """ Return the SQL to delete existing rows of current filter. """
+        return """
+            DELETE FROM filtered_data fd 
+            WHERE EXISTS (
+                SELECT 1 FROM extracted_properties ep 
+                WHERE ep.method_id = :mid
+                AND ep.id = fd.target_id 
+            )
+            AND fd.target_table = :table
+            AND fd.filter_name = :filter;
+        """
 
     def _get_record_sql(self) -> str:
         raise NotImplementedError(self.filter_name)
@@ -54,12 +73,18 @@ class DataValidator:
         raise NotImplementedError(self.filter_name)
 
 
-    def process_items(self, limit : int = None, redo : bool = False):
+    def process_items(self, limit : int = None, redo : bool = False,
+                      remove : bool = True):
         assert self.filter_name
         assert self.table_name
 
         # Init variables
         self.ckpt_name = f"data_{self.filter_name}_{self.method.id}"
+
+        # Remove the existing items
+        if remove:
+            self._delete_existing()
+            redo = True
 
         # Run the pipeline.
         sql = self._get_record_sql()
