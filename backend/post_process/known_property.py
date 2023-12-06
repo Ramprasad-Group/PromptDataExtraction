415b9857cf0b4907460145ff7588bb13cc9000a6
akhlakm <me@akhlakm.com>
2023-12-06 10:10:41 -0500

    Add NERNameValidator, docstrings

--- backend/post_process/known_property.py
+++ backend/post_process/known_property.py
@@ -3,6 +3,9 @@ from backend.post_process.validator import DataValidator
 
 class NameValidator(DataValidator):
     def __init__(self, db, method, meta) -> None:
+        """ For a specific extraction method, identify the rows
+        of extracted_properties having invalid property names.
+        """
         super().__init__(db, method)
 
         # Set required parameters
@@ -31,7 +34,7 @@ class NameValidator(DataValidator):
         """
     
     def _check_filter(self, row) -> bool:
-        """ Return True if row passes the filter. """
+        """ Returns True if row passes the invalid name filter. """
 
         # Lowercase
         name = row.entity_name.lower()
@@ -46,6 +49,9 @@ class NameValidator(DataValidator):
 
 class RangeValidator(DataValidator):
     def __init__(self, db, method, meta) -> None:
+        """ For a specific extraction method, identify the rows
+        of extracted_properties having out of range values.
+        """
         super().__init__(db, method)
 
         # Set required parameters
@@ -75,7 +81,7 @@ class RangeValidator(DataValidator):
         """
     
     def _check_filter(self, row) -> bool:
-        """ Return True if row passes the filter. """
+        """ Return True if row passes the out of range filter. """
 
         criteria = [
             row.value <= self.prop_meta.upper_limit,
@@ -90,6 +96,9 @@ class RangeValidator(DataValidator):
 
 
 class UnitValidator(DataValidator):
+    """ For a specific extraction method, identify the rows
+    of extracted_properties having invalid unit.
+    """
     def __init__(self, db, method, meta) -> None:
         super().__init__(db, method)
 
@@ -120,7 +129,7 @@ class UnitValidator(DataValidator):
         """
     
     def _check_filter(self, row) -> bool:
-        """ Return True if row passes the filter. """
+        """ Return True if row passes the invalid unit filter. """
 
         # Handle None, lowercase
         unit = row.value.lower() if row.value else ""
@@ -137,3 +146,49 @@ class UnitValidator(DataValidator):
         
         log.warn("Invalid property unit: {} ({})", unit, row.id)
         return True
+
+
+class NERNameValidator(DataValidator):
+    def __init__(self, db, method, meta) -> None:
+        """ For a specific property meta information, identify the rows
+        of extracted_properties having matching property names.
+        """
+        super().__init__(db, method)
+
+        # Set required parameters
+        self.filter_name = 'valid_property_name'
+        self.table_name = 'extracted_properties'
+        self.prop_meta = meta
+
+    def _get_record_sql(self) -> str:
+        return """
+            SELECT * FROM (
+                SELECT
+                    ep.id,
+                    ep.entity_name
+                FROM extracted_properties ep
+                -- filter with extraction method
+                WHERE ep.method_id = :mid
+                AND ep.id > :last ORDER BY ep.id
+            ) AS ft
+            -- Ignore previously processed ones
+            WHERE NOT EXISTS (
+                SELECT 1 FROM filtered_data fd 
+                WHERE fd.filter_name = :filter
+                AND fd.target_table = :table
+                AND fd.target_id = ft.id
+            );
+        """
+    
+    def _check_filter(self, row) -> bool:
+        """ Returns True if row passes the matching name filter. """
+
+        # Lowercase
+        name = row.entity_name.lower()
+        namelist = [n.lower() for n in self.prop_meta.other_names]
+
+        if name in namelist:
+            log.info("Matching property name: {} ({})", row.entity_name, row.id)
+            return True
+
+        return False
