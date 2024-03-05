dfa46899402de8b2cf7a7a674c390485bd9ad722
akhlakm <me@akhlakm.com>
2024-03-04 22:04:03 -0500

    Add comments

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,19 @@ author      = 'Akhlak Mahmood, Sonakshi Gupta'
 release     = '0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosummary',
-    'myst_parser',
+    'myst_parser',              # pip install myst_parser
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
