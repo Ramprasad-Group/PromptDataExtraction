86f8f7dfcb9e346a69006e0b9ebdd47123d50ef2
akhlakm <me@akhlakm.com>
2024-03-04 23:33:57 -0500

    Remove test for old module

--- tests/test_dataset.py
+++
@@ -1,8 +0,0 @@
-# Usage: python -m pytest -v tests/test_dataset.py -s
-
-from backend.data.dataset_pranav import GroundDataset
-
-def test_ground_datasets():
-    ds = GroundDataset()
-    gnd, cur = ds.create_dataset('bandgap')
-    assert len(gnd) > 0
